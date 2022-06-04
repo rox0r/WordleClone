82418a34041eb4ad187f936b38a1c0d6996b547e
Raunaq singh <macbook@Raunaqs-MacBook-Air.local>
2022-06-04 08:34:15 +0530

    Add raw base code

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const attemptedWords = [];
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  attemptedWords.push(req.body.word);
+  res.redirect("/");
+});
+
+app.listen(3000, function () {
+  console.log("started listening");
+});
